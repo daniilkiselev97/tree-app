bb0aa7d0427fa1ec910e46a4d51e78a2486140bf
Daniil <dvkiselev@fil-it.ru>
2024-05-14 18:23:21 +0300

    init proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, ChangeDetectorRef } from '@angular/core';
+import { JsonDataService } from './json-data.service';
+// import { TuiTreeModule } from '@taiga-ui/components';
+
+interface TreeNode {
+  id: string;
+  name: string;
+  children: TreeNode[];
+}
+// interface Department {
+//   name: string;
+//   children?: Department[];
+// }
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tree-app';
+// export class AppComponent implements OnInit {
+// 	data: TreeNode[] = []
+
+// 	constructor(
+// 		private readonly _jsonDataService: JsonDataService,
+// 		private readonly _cdr: ChangeDetectorRef
+// 	){}
+
+// 	ngOnInit(): void {
+// 		this._jsonDataService.getData().subscribe((data: TreeNode[]) => {
+//       console.log(this.data); 
+// 			this.data = data
+//       this._cdr.markForCheck(); 
+//     });
+//   }
+  
+//   trackById(index: number, item: TreeNode): string {
+//     return item.id;
+//   }
+// }
+
+export class AppComponent implements OnInit {
+  data: TreeNode[] = [];
+
+  constructor(
+    private readonly jsonDataService: JsonDataService,
+    private readonly cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.jsonDataService.getData().subscribe((data: TreeNode[]) => {
+      console.log(data);
+      this.data = this.removeDuplicates(data);
+      this.cdr.markForCheck();
+    });
+  }
+
+  trackById(index: number, item: TreeNode): string {
+    return item.id;
+  }
+
+  removeDuplicates(nodes: TreeNode[]): TreeNode[] {
+		const seenIds: Set<string> = new Set();
+		const filteredNodes: TreeNode[] = [];
+		nodes.forEach(node => this.filterNode(node, seenIds, filteredNodes));
+		return filteredNodes;
+	}
+	
+	filterNode(node: TreeNode, seenIds: Set<string>, filteredNodes: TreeNode[]): void {
+  if (seenIds.has(node.id)) {
+    return; // Если узел уже был обработан, просто завершаем выполнение
+  }
+  seenIds.add(node.id); // Добавляем идентификатор узла в множество
+  filteredNodes.push(node); // Добавляем узел в массив уникальных узлов
+
+  // Рекурсивно обрабатываем дочерние узлы
+  node.children.forEach(child => this.filterNode(child, seenIds, filteredNodes));
 }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TuiTreeModule} from '@taiga-ui/kit';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+      BrowserAnimationsModule,
+      TuiRootModule,
+      TuiDialogModule,
+      TuiAlertModule,
+			TuiTreeModule,
+			HttpClientModule
+],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
