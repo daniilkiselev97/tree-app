b9323f7e1181938012054a5f84177c196a44bc06
Daniil <dvkiselev@fil-it.ru>
2024-05-21 12:26:28 +0300

    fix: directive-highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,19 +1,43 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, AfterViewInit, AfterViewChecked, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
-export class HighlightDirective implements OnInit {
+export class HighlightDirective implements OnChanges, AfterViewInit, AfterViewChecked {
   @Input('highlight') searchTerm: string = '';
+
+  private previousSearchTerm: string = '';
 
   constructor(private el: ElementRef) {}
 
-  ngOnInit(): void {
-    if (this.searchTerm) {
-      const regex = new RegExp(`(${this.searchTerm})`, 'gi');
-      const htmlContent = this.el.nativeElement.innerHTML;
-      const highlightedContent = htmlContent.replace(regex, '<span class="highlight">$1</span>');
-      this.el.nativeElement.innerHTML = highlightedContent;
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if (changes['searchTerm']) {
+      this.highlightText();
     }
   }
+
+  ngAfterViewInit(): void {
+    this.highlightText();
+  }
+
+  ngAfterViewChecked(): void {
+    if (this.previousSearchTerm !== this.searchTerm) {
+      this.highlightText();
+      this.previousSearchTerm = this.searchTerm;
+    }
+  }
+
+	private highlightText(): void {
+    const textContent = this.el.nativeElement.textContent;
+    if (this.searchTerm && textContent) {
+        const regex = new RegExp(`(${this.searchTerm})`, 'gi');
+        const highlightedContent = textContent.replace(regex, '<span class="highlight">$1</span>');
+        this.el.nativeElement.innerHTML = highlightedContent;
+    }
 }
+
+}
+
+
+
